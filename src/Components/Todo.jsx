e334ff63cb001b956589e46050d1370ba23296aa
MrUnwonted <arjungodzarjun@gmail.com>
2023-08-02 01:51:37 +0530

    Todo almost

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Todo.css'
 import { useState, useRef, useEffect} from 'react'
+import {IoMdDoneAll} from 'react-icons/io'
+import {FiEdit} from 'react-icons/fi'
+import {MdDelete} from 'react-icons/md'
+
+
 
 
 function Todo() {
@@ -8,7 +13,7 @@ function Todo() {
   const[todos, setTodos] = useState([])
  
   const addTodo = () => {
-    setTodos([...todos,todo])
+    setTodos([...todos,{list: todo, id : Date.now(), status : false}])
     console.log(todos);
     setTodo('')
   }
@@ -23,6 +28,25 @@ function Todo() {
     inputRef.current.focus()
   })
 
+  const onDelete = (id) => {
+    setTodos(todos.filter((to) => to.id !== id))
+  }
+
+  const onComplete = (id) => {
+    let complete = todos.map((list) =>{
+      if(list.id === id){
+        return ({...list, status: !list.status})
+      }
+      return list
+    } )
+    setTodos(complete)
+  }
+
+  const onEdit = (id) => {
+    const editTodo = todos.find((to) => to.id === id)
+    setTodo(editTodo.list)
+  }
+
 
   return (
     <div className='container'>
@@ -35,7 +59,23 @@ function Todo() {
             <ul>
                 {
                   todos.map((to) =>(
-                      <li> {to} </li>
+                      <li className='list-items'>
+                         <div className="list-item-list" id= {to.status?'list-item' : ''} >{to.list}</div>
+                            <span>
+                                <IoMdDoneAll 
+                                  className='list-item-icons'
+                                  id='complete'
+                                  title='Complete'
+                                  onClick={() => onComplete(to.id)} />    
+                                <FiEdit className='list-item-icons' id='edit' title='Edit' 
+                                  onClick={() => onEdit(to.id)}/>
+                                <MdDelete 
+                                  className='list-item-icons'
+                                   id='delete'
+                                   title='Delete'
+                                   onClick={() => onDelete(to.id)} />
+                            </span>
+                       </li>
                   ))
                 }
 
